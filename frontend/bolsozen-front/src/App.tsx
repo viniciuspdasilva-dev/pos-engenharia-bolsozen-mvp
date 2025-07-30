7a82a61a422a4f8aca71473ab4de266fa054cf38
Vinicius Silva <viniciuspdasilva.dev@gmail.com>
2025-07-30 18:58:49 -0300

    Add initial React + TypeScript frontend setup
    - Scaffolded a new frontend project using Vite with React and TypeScript.
    - Added Tailwind CSS for styling with a basic configuration.
    - Created `.gitignore` files for project exclusions.
    - Implemented a basic App structure with `App.tsx` and global styles (`App.css`, `index.css`).
    - Configured ESLint with TypeScript and React plugins.
    - Added a sample `index.html` and linked the React app entry point (`main.tsx`).
    - Included all necessary dependencies and lock files (`package.json`, `package-lock.json`).

---
+++ frontend/bolsozen-front/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-3xl font-bold underline">
+          Teste do tailwind css
+      </div>
+    </>
+  )
+}
+
+export default App
